d283dec0dae0d3641284bace9bc0336910e2eeaf
DannyEdelbroek <334855@student.mboutrecht.nl>
2025-05-18 18:41:20 +0200

    Add files via upload

---
+++ Http/controller/accounts/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$accounts = $db->query("
+    SELECT 
+        Gebruikersnaam,
+        CONCAT(gebruiker.Voornaam, ' ', 
+               COALESCE(gebruiker.Tussenvoegsel, ''), ' ',
+               gebruiker.Achternaam) AS VolledigeNaam
+    FROM Gebruiker
+")->get();
+
+view("accounts/index.view.php", [
+    'accounts' => $accounts,
+    'heading' => 'Accounten'
+]);
+
