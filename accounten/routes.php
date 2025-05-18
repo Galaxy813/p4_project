ce1f0541752e45363c4cd5233661882c2194f890
DannyEdelbroek <334855@student.mboutrecht.nl>
2025-05-18 18:31:22 +0200

    Fifth commit

---
+++ accounten/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+//alle main routes dat zit op de pagina!
+$router->get('/', 'index.php');
+$router->get('/calendar', 'calendar.php');
+$router->get('/contact', 'contact.php');
+
+$router->get('/notes', 'notes/index.php')->only('auth');
+
+$router->get('/note/create', 'notes/create.php');
+
+$router->get('/register', 'Regristration/create.php')->only('guest');
+$router->post('/register', 'Regristration/store.php');
+
+$router->get('/login', 'sessions/create.php')->only('guest');
+$router->post('/sessions', 'sessions/store.php')->only('guest');
+$router->get('/logout', 'sessions/logout.php');
+
+$router->get('/accounts/create', 'accounts/index.php')->only('auth');
+$router->get('/accounts', 'accounts/create.php');
+
+
+
+
+
+
+// $router->get('/note', 'notes/show.php');
+// $router->delete('/note', 'notes/destroy.php');
+
+// $router->get('/note/edit', 'notes/edit.php');
+// $router->patch('/note', 'notes/update.php');
+
+// $router->post('/notes', 'notes/store.php');
